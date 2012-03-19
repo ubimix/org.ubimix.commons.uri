98bf8e2cdce7fda529b9e353a5c570a75f779823
mikhail.kotelnikov <none@none>
2012-03-19 16:40:44 +0100

    Copying of query parameteres was fixed.

--- src/main/java/org/webreformatter/commons/uri/Uri.java
+++ src/main/java/org/webreformatter/commons/uri/Uri.java
@@ -1,21 +1,18 @@
 /* ************************************************************************** *
  * See the NOTICE file distributed with this work for additional information
- * regarding copyright ownership.
- * 
- * This file is licensed to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- * ************************************************************************** */
+ * regarding copyright ownership. This file is licensed to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * **************************************************************************
+ */
 package org.webreformatter.commons.uri;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -74,7 +71,10 @@ public class Uri extends AbstractUri {
         fFragment = uri.getFragment();
         fHost = uri.getHost();
         fPort = uri.getPort();
-        fQueryItems = Collections.unmodifiableList(uri.getQueryItems());
+        List<QueryItem> queryItems = uri.getQueryItems();
+        fQueryItems = queryItems != null
+            ? Collections.unmodifiableList(queryItems)
+            : new ArrayList<AbstractUri.QueryItem>();
         fSchemeSegments = Collections.unmodifiableList(uri.getSchemeSegments());
         fUserInfo = uri.getUserInfo();
     }
